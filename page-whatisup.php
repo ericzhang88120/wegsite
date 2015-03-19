030b3db98d026d35a966aca27e551ac008da02da
Clare <sea11257@mail.ustc.edu.cn>
2015-03-20 01:20:05 +0800

    temp

---
+++ page-whatisup.php
@@ -0,0 +1,96 @@
+<?php get_header(); ?>
+<div class="container">
+	<div>
+		<img src="wp-content/themes/wegsite/images/Temp/image1.jpg">
+	</div>
+</div>
+<div class="container" style="background-image:url(wp-content/themes/wegsite/images/Temp/image2.jpg)">
+
+	<div class="row" style="padding-top:140px;text-align:right">
+		<div class="col-lg-1 col-md-1 col-sm-1">
+		</div>
+		<div class="col-lg-10 col-md-10 col-sm-10">
+			<a href="#">
+				<button style="border:0px solid;width:100px;height:30px;background-image:url(wp-content/themes/wegsite/images/Temp/more.png)">
+				</button>
+			</a>
+		</div>
+		<div class="col-lg-1 col-md-1 col-sm-1">
+		</div>
+	</div>
+	
+	<div class="row" style="padding-top:0px">
+		<div class="col-lg-1 col-md-1 col-sm-1">
+		</div>
+		<div class="col-lg-2 col-md-2 col-sm-2" style="overflow:hidden">
+			<img src="wp-content/themes/wegsite/images/Temp/image3.jpg" style="padding-top:10px">
+		</div>
+		<div class="col-lg-8 col-md-8 col-sm-8">
+			<div class="row" style="overflow:hidden">
+				<img src="wp-content/themes/wegsite/images/Temp/line.png">
+			</div>
+			<div class="row" style="overflow:hidden">
+				<font class="star_desc" size="5px"><b>PRESTIGIOUS GLOBAL ACTION SPORTS COMPETITION STAGED IN SHANGHAI FOR THE NINTH CONSECUTIVE YEAR<br></b></font>
+				<font size="3px" color="#d0d0d0">New Sports, Multiple Venues and World-Class Athletes; Mat Hoffman, Trey Wood, Jimmy Wilkins and Filipe Gustavo Among the Elite to Attend from April 25 - May 3...</font>
+			</div>
+			<div class="row" style="overflow:hidden;text-align:right">
+				<a href="?page_id=9">
+					<button style="border:0px solid;width:100px;height:30px;background-image:url(wp-content/themes/wegsite/images/Temp/read.png)">
+					</button>
+				</a>
+			</div>
+		</div>
+		<div class="col-lg-1 col-md-1 col-sm-1">
+		</div>
+	</div>
+	<div class="row" style="padding-top:10px">
+		<div class="col-lg-1 col-md-1 col-sm-1">
+		</div>
+		<div class="col-lg-2 col-md-2 col-sm-2" style="overflow:hidden">
+			<img src="wp-content/themes/wegsite/images/Temp/image4.png" style="padding-top:10px">
+		</div>
+		<div class="col-lg-8 col-md-8 col-sm-8">
+			<div class="row" style="overflow:hidden">
+				<img src="wp-content/themes/wegsite/images/Temp/line.png">
+			</div>
+			<div class="row" style="overflow:hidden">
+				<font class="star_desc" size="5px"><b>SHANGHAI TO HOST THE WORLD'S BEST ACTION SPORTS ATHLETES AT THE KIA WORLD EXTREME GAMES INCLUDING MAT HOFFMAN AND CHRIS COLE<br></b></font>
+				<font size="3px" color="#d0d0d0">New Sports, Multiple Venues, World-Class Athletes; BMX and Skateboard Legends Dennis McCoy and Tom Asta Also Among the Elite to Attend April 25 - May 3...</font>
+			</div>
+			<div class="row" style="overflow:hidden;text-align:right">
+				<a href="#">
+					<button style="border:0px solid;width:100px;height:30px;background-image:url(wp-content/themes/wegsite/images/Temp/read.png)">
+					</button>
+				</a>
+			</div>
+		</div>
+		<div class="col-lg-1 col-md-1 col-sm-1">
+		</div>
+	</div>
+	<div class="row" style="padding-top:10px">
+		<div class="col-lg-1 col-md-1 col-sm-1">
+		</div>
+		<div class="col-lg-2 col-md-2 col-sm-2" style="overflow:hidden">
+			<img src="wp-content/themes/wegsite/images/Temp/image4.png" style="padding-top:10px">
+		</div>
+		<div class="col-lg-8 col-md-8 col-sm-8">
+			<div class="row" style="overflow:hidden">
+				<img src="wp-content/themes/wegsite/images/Temp/line.png">
+			</div>
+			<div class="row" style="overflow:hidden">
+				<font class="star_desc" size="4px"><b>HAND IN THERE...<br></b></font>
+			</div>
+			<div class="row" style="overflow:hidden;text-align:right">
+			</div>
+		</div>
+		<div class="col-lg-1 col-md-1 col-sm-1">
+		</div>
+	</div>
+
+</div>
+<div class="container">
+	<div>
+		<img src="wp-content/themes/wegsite/images/Temp/image5.jpg">
+	</div>
+</div>
+<?php get_footer(); ?>
