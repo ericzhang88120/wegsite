6d72cdb2dde9937678cce393435d78cb4f1bd008
Clare <sea11257@mail.ustc.edu.cn>
2015-03-25 23:08:23 +0800

    add the post of what is up

--- page-whatisup.php
+++ page-whatisup.php
@@ -1,16 +1,28 @@
+<?php $wl_theme_options = weblizar_get_options(); ?>
+
 <?php get_header(); ?>
-<div class="container"style="background-image:url(wp-content/themes/wegsite/images/Temp/image1.jpg);height:549px;background-repeat:no-repeat">
-	<div>
+
+<div class="container" style="background-image:url(<?php echo esc_url($wl_theme_options['page_what_is_up_background_1']); ?>);background-repeat:no-repeat;height:550px;">
+	<div class="row" style="padding-top:160px;text-align:right">
+		<div class="col-lg-6 col-md-6 col-sm-6">
+			<a href="#">
+				<img src="<?php echo esc_url($wl_theme_options['page_what_is_up_image_1']); ?>" alt="<?php the_title_attribute(); ?>">
+			</a>
+		</div>
+		<div class="col-lg-6 col-md-6 col-sm-6" style="overflow:hidden;text-align:left;">
+			<font size="0.5px" color="#c0c0c0">//////////////////////////////////////////////////////////////////////////////////////////////////////////<br></font>
+			<font size="2px" color="#ffffff">Hang in there...</font>
+		</div>
 	</div>
 </div>
-<div class="container" style="background-image:url(wp-content/themes/wegsite/images/Temp/image2.jpg)">
+<div class="container" style="background-image:url(<?php echo esc_url($wl_theme_options['page_what_is_up_background_2']); ?>);background-repeat:no-repeat;height:685px;">
 
 	<div class="row" style="padding-top:140px;text-align:right">
 		<div class="col-lg-1 col-md-1 col-sm-1">
 		</div>
 		<div class="col-lg-10 col-md-10 col-sm-10">
 			<a href="#">
-				<button style="border:0px solid;width:100px;height:30px;background-image:url(wp-content/themes/wegsite/images/Temp/more.png)">
+				<button style="border:0px solid;width:100px;height:30px;background-image:url(<?php echo esc_url($wl_theme_options['page_what_is_up_more']); ?>)">
 				</button>
 			</a>
 		</div>
@@ -22,11 +34,11 @@
 		<div class="col-lg-1 col-md-1 col-sm-1">
 		</div>
 		<div class="col-lg-2 col-md-2 col-sm-2" style="overflow:hidden">
-			<img src="wp-content/themes/wegsite/images/Temp/image3.jpg" style="padding-top:10px">
+			<img src="<?php echo esc_url($wl_theme_options['page_what_is_up_image_2']); ?>" style="padding-top:10px">
 		</div>
 		<div class="col-lg-8 col-md-8 col-sm-8">
 			<div class="row" style="overflow:hidden">
-				<img src="wp-content/themes/wegsite/images/Temp/line.png">
+				<img src="<?php echo esc_url($wl_theme_options['page_what_is_up_line']); ?>">
 			</div>
 			<div class="row" style="overflow:hidden">
 				<font class="star_desc" size="5px"><b>NEW SPORTS, MULTIPLE VENUES,WORLD-CLASS ATHLETES; BMX AND SKATEBOARD LEGENDS DENNIS...<br></b></font>
@@ -34,7 +46,7 @@
 			</div>
 			<div class="row" style="overflow:hidden;text-align:right">
 				<a href='<?php echo get_permalink( get_page_by_title( 'media1' ) );?>'>
-					<button style="border:0px solid;width:100px;height:30px;background-image:url(wp-content/themes/wegsite/images/Temp/read.png)">
+					<button style="border:0px solid;width:100px;height:30px;background-image:url(<?php echo esc_url($wl_theme_options['page_what_is_up_read']); ?>)">
 					</button>
 				</a>
 			</div>
@@ -46,11 +58,11 @@
 		<div class="col-lg-1 col-md-1 col-sm-1">
 		</div>
 		<div class="col-lg-2 col-md-2 col-sm-2" style="overflow:hidden">
-			<img src="wp-content/themes/wegsite/images/Temp/image6.jpg" style="padding-top:10px">
+			<img src="<?php echo esc_url($wl_theme_options['page_what_is_up_image_3']); ?>" style="padding-top:10px">
 		</div>
 		<div class="col-lg-8 col-md-8 col-sm-8">
 			<div class="row" style="overflow:hidden">
-				<img src="wp-content/themes/wegsite/images/Temp/line.png">
+				<img src="<?php echo esc_url($wl_theme_options['page_what_is_up_line']); ?>">
 			</div>
 			<div class="row" style="overflow:hidden">
 				<font class="star_desc" size="5px"><b>PRESTIGIOUS GLOBAL ACTION SPORTS COMPETITION STAGED IN SHANGHAI FOR THE NINTH CONSECUTIVE YEAR...<br></b></font>
@@ -58,7 +70,7 @@
 			</div>
 			<div class="row" style="overflow:hidden;text-align:right">
 				<a href='<?php echo get_permalink( get_page_by_title( 'media2' ) );?>'>
-					<button style="border:0px solid;width:100px;height:30px;background-image:url(wp-content/themes/wegsite/images/Temp/read.png)">
+					<button style="border:0px solid;width:100px;height:30px;background-image:url(<?php echo esc_url($wl_theme_options['page_what_is_up_read']); ?>)">
 					</button>
 				</a>
 			</div>
@@ -70,11 +82,11 @@
 		<div class="col-lg-1 col-md-1 col-sm-1">
 		</div>
 		<div class="col-lg-2 col-md-2 col-sm-2" style="overflow:hidden">
-			<img src="wp-content/themes/wegsite/images/Temp/image4.png" style="padding-top:10px">
+			<img src="<?php echo esc_url($wl_theme_options['page_what_is_up_image_4']); ?>" style="padding-top:10px">
 		</div>
 		<div class="col-lg-8 col-md-8 col-sm-8">
 			<div class="row" style="overflow:hidden">
-				<img src="wp-content/themes/wegsite/images/Temp/line.png">
+				<img src="<?php echo esc_url($wl_theme_options['page_what_is_up_line']); ?>">
 			</div>
 			<div class="row" style="overflow:hidden">
 				<font class="star_desc" size="4px"><b>HAND IN THERE...<br></b></font>
@@ -87,8 +99,32 @@
 	</div>
 
 </div>
-<div class="container"style="background-image:url(wp-content/themes/wegsite/images/Temp/image5.jpg);height:800px;background-repeat:no-repeat">
-	<div>
+<div class="container" style="background-image:url(<?php echo esc_url($wl_theme_options['page_what_is_up_background_3']); ?>);background-repeat:no-repeat;height:746px;">
+	<div class="row" style="padding-top:140px;text-align:right;padding-right:10px">
+		<a href="#">
+			<img style="border:0px solid;width:48px;height:18px;background-image:url(<?php echo esc_url($wl_theme_options['page_what_is_up_year']); ?>)">
+			</img>
+		</a>
+	</div>
+	<div class="row" style="padding-top:5px;padding-right:10px">
+		<div class="col-lg-3 col-md-3 col-sm-3">
+			<div class="row">
+				<img src="<?php echo esc_url($wl_theme_options['page_what_is_up_image_5']);?>">
+				<img src="<?php echo esc_url($wl_theme_options['page_what_is_up_image_6']);?>">
+			</div>
+			<div class="row" style="padding-top:5px;">
+			<img src="<?php echo esc_url($wl_theme_options['page_what_is_up_image_7']);?>">
+			<img src="<?php echo esc_url($wl_theme_options['page_what_is_up_image_8']);?>">
+			</div>
+			<div class="row" style="padding-top:5px;">
+			<img src="<?php echo esc_url($wl_theme_options['page_what_is_up_image_9']);?>">
+			<img src="<?php echo esc_url($wl_theme_options['page_what_is_up_image_10']);?>">
+			</div>
+		</div>
+		<div class="col-lg-9 col-md-9 col-sm-9" style="overflow:hidden">
+			<img src="<?php echo esc_url($wl_theme_options['page_what_is_up_image_11']);?>">
+			<font style="position:absolute;top:50%; right:50%;z-index:10;" class="star_desc" size="5px"><b>HAND IN THERE...<br></b></font>
+		</div>
 	</div>
 </div>
 <?php get_footer(); ?>
